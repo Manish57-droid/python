7db491a4164e0e0c2b60e41ad1a6405288daf1a1
Kaliro26 <115157729+Kaliro26@users.noreply.github.com>
2022-10-08 20:10:02 +0530

    Hacktoberfest 2022

---
+++ positive-or-negative-or-zero (1).cpp
@@ -0,0 +1,19 @@
+// 6. Write a Program to find whether the given number is 
+//  positive or negative or zero. 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the value of n is : ";
+    cin>>n;
+    if(n>0)
+    cout<<"positive";
+    else if(n<0)
+    cout<<"negative";
+    else
+    cout<<"zero";
+}
