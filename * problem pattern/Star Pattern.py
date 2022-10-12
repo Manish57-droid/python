1e912f03226f1a20036d8258d0f42027eaee0004
Sravan Sainath <94300165+JSravanreddy@users.noreply.github.com>
2022-10-12 17:20:01 +0530

    Added a bit manupulation code--- Gray code sequence

--- * problem pattern/Star Pattern.py
+++
@@ -1,5 +0,0 @@
-num=int(input("enter any number"))
-for i in range(num):
-    for j in range(i):
-        print("*",end=" ")
-    print()
--- pythonterm.py
+++
@@ -1,1 +0,0 @@
-print(" H a c k t o b e r f e s t 2 0 2 2")
