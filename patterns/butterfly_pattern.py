2000e960fe3081c6234f7ff8ab8dd6ecf07fb0b9
MEGHWANT SINGH <68533246+Meghwantsingh@users.noreply.github.com>
2022-10-18 20:07:44 +0530

    butterfly_pattern.py
    butterfly pattern using stars in Python
    butterfly_pattern.py file is added for hacktoberfest add it. label it

---
+++ patterns/butterfly_pattern.py
@@ -0,0 +1,29 @@
+# r total Number of rows
+r = 5
+
+# Upper Triangles
+for i in range(1, r+1):
+    print("*"*i, end="")
+    print(" "*(r-i)*2, end="")
+    print("*"*i)
+
+# Lower Triangles
+for i in range(r,0,-1):
+    print("*"*i, end="")
+    print(" "*(r-i)*2, end="")
+    print("*"*i)    
+
+"""
+output:
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+**********
+****  ****
+***    ***
+**      **
+*        *
+"""
