9fdec0d868261ab451e2d557f01b94920963fd53
Shubham Jain <71687770+jainshubham766@users.noreply.github.com>
2022-10-20 13:47:28 +0530

    Pyramid Pattern Of Alternate Numbers
    Pyramid Pattern Of Alternate Numbers for hacktoberfeast

---
+++ PyramidPatternOfAlternateNumbers.py
@@ -0,0 +1,27 @@
+rows = 5
+i = 1
+while i <= rows:
+    j = 1
+    while j <= i:
+        print(((i * 2) - 1), end="")
+        j = j + 1
+    i = i + 1
+   print()
+  
+  
+  """
+  
+  Expected O/P
+  
+1 
+
+3 3 
+
+5 5 5 
+
+7 7 7 7 
+
+9 9 9 9 9
+
+
+  """
