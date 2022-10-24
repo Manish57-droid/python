a8554a4d20eab3f36044ce59c1152be0a8b63190
Sajal-24-jain <96467219+Sajal-24-jain@users.noreply.github.com>
2022-10-24 23:56:40 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,12 @@
+def gcd(x, y):
+   gcd = 1   
+   if x % y == 0:
+       return y   
+   for k in range(int(y / 2), 0, -1):
+       if x % k == 0 and y % k == 0:
+           gcd = k
+           break 
+   return gcd
+print("GCD of 12 & 17 =",gcd(12, 17))
+print("GCD of 4 & 6 =",gcd(4, 6))
+print("GCD of 336 & 360 =",gcd(336, 360))
